7cc6ba02ec02232a6ca593fe36b0e3603068bbaf
Nikolas Furuta <nikolasfuruta@gmail.com>
2022-07-06 00:42:08 -0300

    refactor: refact obterCod function

--- src/util/axios/ObterCod.ts
+++ src/util/axios/ObterCod.ts
@@ -1,17 +1,18 @@
 import axios , { AxiosResponse } from "axios";
 import { load } from 'cheerio';
 
-export default async function obterCod(estado: string, municipio?: string): Promise<string | undefined> {
+export default async function obterCod(estado: string, municipio?: string): Promise<string | undefined>{
+  let url: string;
   if(municipio != undefined) {
-    return await codMunicipio(estado, municipio)
+    url = `https://www.ibge.gov.br/cidades-e-estados/${estado}/${municipio}.html`;
   }
   else {
-    await codEstado(estado)
+    url = `https://www.ibge.gov.br/cidades-e-estados/${estado}`;
   }
+  return await gerarCod(url)
 }
 
-export async function codMunicipio(estado: string, municipio: string){
-  const url = `https://www.ibge.gov.br/cidades-e-estados/${estado}/${municipio}.html`
+export async function gerarCod(url: string): Promise<string | undefined> {
   try{
     const response: AxiosResponse = await axios(url)
     const html: string = response.data;
@@ -24,18 +25,4 @@ export async function codMunicipio(estado: string, municipio: string){
   finally { console.log("concluído") }
 }
 
-export async function codEstado(estado: string){
-  const url = `https://www.ibge.gov.br/cidades-e-estados/${estado}`;
-  try{
-    const response: AxiosResponse = await axios(url)
-    const html: string = response.data;
-    const $ = load(html);
-    const tag = $('#responseMunicipios');
-    const cod = tag.attr('data-codigo');
-    return cod
-  }
-  catch(e) { console.error(e) }
-  finally { console.log("concluído") }
-}
-
-obterCod("SP")
+//obterCod("SP")
