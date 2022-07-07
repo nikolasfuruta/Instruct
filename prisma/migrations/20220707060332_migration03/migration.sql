7ce98354959ddb5a611af443849c204f8feec4f8
Nikolas Furuta <nikolasfuruta@gmail.com>
2022-07-07 03:07:41 -0300

    feat: add consultar method in the FeriadoService

---
+++ prisma/migrations/20220707060332_migration03/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "FeriadoEstadual" ALTER COLUMN "dataFeriado" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "FeriadoMunicipal" ALTER COLUMN "dataFeriado" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "FeriadoNacional" ALTER COLUMN "dataFeriado" SET DATA TYPE TEXT;
--- src/service/FeriadoService.ts
+++ src/service/FeriadoService.ts
@@ -7,6 +7,11 @@ export default class FeriadoService {
   }
 
   public static async consultar(cod: string, data: string) {
-    return {cod, data}
+    if(cod.length === 2) {
+      return await prisma.feriadoEstadual.findFirst({ where: { dataFeriado: data } });
+    }
+    else {
+      return await prisma.feriadoMunicipal.findFirst({ where: { dataFeriado: data } });
+    }
   }
 }
